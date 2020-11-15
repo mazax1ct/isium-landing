0af4ebc02e7636f3b738a3a4c305e22dcab8d0be
mazax1ct <vova@giveandget.ru>
2020-11-15 03:24:52 +0300

    loyal

--- build/js/menu.js
+++ build/js/menu.js
@@ -1,9 +1,18 @@
-let toggler = document.querySelector('.js-menu-toggler'),
-    menu = document.querySelector('.menu');
-
-function menuToggle() {
-  menu.classList.toggle('is-open');
-  return false;
+function menuClose(evt) {
+  if (!$('.menu').is(evt.target) && $('.menu').has(evt.target).length === 0) {
+    $('.menu').removeClass('is-open');
+    document.removeEventListener('click', menuClose);
+    console.log('удаляем');
+  }
 };
 
-toggler.addEventListener('click', menuToggle);
+$(document).on('click', '.js-menu-toggler', function () {
+  if($('.menu').hasClass('is-open')) {
+    $('.menu').removeClass('is-open');
+  } else {
+    $('.menu').addClass('is-open');
+    document.addEventListener('click', menuClose);
+    console.log('добавляем');
+  }
+  return false;
+});
